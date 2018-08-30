48fb8b25380dc7ed2112efaebb18c76ad60be13b
Ross Wilson <ross.wilson@otago.ac.nz>
2018-08-30 18:06:52 +1200

    revise overlay positioning

--- www/js.js
+++ www/js.js
@@ -44,11 +44,11 @@ button_expand.addEventListener("click", function(){
 //---------- Add behavior to overlay page-----------//
 function on() {
     document.getElementById("overlay").style.display = "block";
-    document.body.style.overflow = "hidden";
+//    document.body.style.overflow = "hidden";
 }
 function off() {
     document.getElementById("overlay").style.display = "none";
-    document.body.style.overflow = "auto";
+//    document.body.style.overflow = "auto";
 }
 
 //---------- Connect slider with numeric inputs ----//
